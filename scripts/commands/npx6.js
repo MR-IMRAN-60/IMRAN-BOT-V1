ddec06b9f444d01d9343247ab12e15649d51d3fa
MR-IMRAN-60 <137718252+MR-IMRAN-60@users.noreply.github.com>
2024-02-15 12:12:58 +0600

    Create npx6.js

---
+++ scripts/commands/npx6.js
@@ -0,0 +1,40 @@
+module.exports.config = {
+  name: "🤩",
+  version: "0.0.2",
+  permission: 0,
+  prefix: false,
+  credits: "Imran",
+  description: "sad video",
+  category: "admin",
+  usages: "",
+    cooldowns: 5,
+};
+
+
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+    if (event.body.indexOf("😍")==0 || (event.body.indexOf("😍")==0) || event.body.indexOf("😍")==0 ||
+event.body.indexOf("😍")==0 ||
+event.body.indexOf("😍")==0 ||
+event.body.indexOf("😍")==0 ||
+event.body.indexOf("😍")==0 ||
+event.body.indexOf("😍")==0 ||
+event.body.indexOf("😍")==0 ||
+event.body.indexOf("😍")==0) {
+    const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+    var link = [
+"https://i.imgur.com/8qtTt4N.mp4",
+        ];
+     var callback = () => api.sendMessage({body:`•—»✨ [ 𝐏𝐫𝐞𝐟𝐢𝐱 𝐄𝐯𝐞𝐧𝐭 ] ✨«—•\n•┄┅════❁🌺❁════┅┄•\n\n ─༅༎•🌺🌸༅༎•─\n
+─༅༎•🥀🌺-Every Muslims dream _______🦋࿐\n\n
+❞༎༊দেখলেই মন জুরিয়ে যায়༅༎•─ \n\n•┄┅════❁🌺❁════┅┄•\n•—»✨[ 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐈𝐦𝐫𝐚𝐧 ]✨«—•\n[🐰] → 𝗣𝗿𝗲𝗳𝗶𝘅 : [ ${PREFIX} ]\n[🫰] 𝐍𝐎𝐏𝐑𝐄𝐅𝐈𝐗  : 😍\n[⌛] → 𝐃𝐚𝐭𝐞  : ${Date.now() - timeStart} 𝐓𝐢𝐦𝐞\n[🍒] → ${global.config.BOTNAME}`,attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+  const timeStart = Date.now();
+  const dcm = process.uptime(); 
+ var anh = Math.floor(dcm / (60 * 60));
+	var la = Math.floor((dcm % (60 * 60)) / 60);
+	var vt = Math.floor(dcm % 60);
+const res = await
+axios.get(`http://toan-culi.tutoan205.repl.co/text/cadao`);
+var thinh = res.data.url;
+let ext = res.data.data.substring(res.data.url.lastIndexOf(".") + 1);
