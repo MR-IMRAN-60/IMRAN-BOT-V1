5997f3e3965a8a1f04071f72542791ba5f72058d
MR-IMRAN-60 <137718252+MR-IMRAN-60@users.noreply.github.com>
2024-02-15 08:41:37 +0600

    Update imu.js

--- scripts/commands/imu.js
+++ scripts/commands/imu.js
@@ -1,1 +1,39 @@
+module.exports.config = {
+    name: "imu",
+    version: "1.0.5",
+    permission: 0,
+    prefix: true,
+    credits: "Imran",
+    description: "sim",
+    category: "prefix",
+    cooldowns: 2
+};
 
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios");
+    let { messageID, threadID, senderID, body } = event;
+    let tid = threadID,
+    mid = messageID;
+    const content = encodeURIComponent(args.join(" "));
+    if (!args[0]) return api.sendMessage(" hm bolo bby😸 ...", tid, mid);
+    try {
+        const res = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=bn&message=${content}&filter=true`);
+        const respond = res.data.success;
+        if (res.data.error) {
+            api.sendMessage(`Error: ${res.data.error}`, tid, (error, info) => {
+                if (error) {
+                    console.error(error);
+                }
+            }, mid);
+        } else {
+            api.sendMessage(respond, tid, (error, info) => {
+                if (error) {
+                    console.error(error);
+                }
+            }, mid);
+        }
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while fetching the data.", tid, mid);
+    }
+};
